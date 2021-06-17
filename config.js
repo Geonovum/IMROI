13581bef197ece424eaa9cae71093bbbd09e53a5
Arnoud de Boer <a.deboer@geonovum.nl>
2021-06-17 13:56:22 +0200

    initiële Respec versie

---
+++ config.js
@@ -0,0 +1,31 @@
+var respecConfig = {
+  format: "markdown",
+  specStatus: "GN-WV",
+  specType: "IM",
+ // subtitle: "Basisregistratie grootschalige topografie",
+  //canonicalURI: "WV",
+  //publishDate: "2017-09-12",
+  editors: [
+    {
+      name: "Arnoud de Boer",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/",
+      mailto: "a.deboer@geonovum.nl",
+      note: ""    
+    }    
+  ],
+  //shortName: "shortname",
+  pubDomain: "IMROI",
+  github: "https://github.com/Geonovum/IMROI",
+  //previousPublishDate: "2017-01-01",
+  //previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
