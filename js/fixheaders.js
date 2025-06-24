320eeb54c32cbd0a567e6754ccac6e78320deed2
Arnoud de Boer <a.deboer@geonovum.nl>
2025-06-24 20:16:47 +0200

    Update fixheaders.js

--- js/fixheaders.js
+++ js/fixheaders.js
@@ -1,30 +1,81 @@
-const dl = document.querySelector('dl');
+document.getElementsByTagName('body')[0].onload = function () {	
 
-console.log(respecConfig);
-// Maak de nieuwe elementen aan
-const items = [
-    { term: 'Deze versie:', url: 'https://docs.geostandaarden.nl/imroi/cv-im-imroi-20240521' },
-    { term: 'Vorige versie:', url: 'https://docs.geostandaarden.nl/imroi/cv-im-imroi-20240430' }, // Vul hier de juiste link in
-    { term: 'Laatst gepubliceerde versie:', url: 'https://docs.geostandaarden.nl/imroi/' }
-];
+	if (typeof respecConfig == 'undefined' || respecConfig.specStatus == 'WV' || respecConfig.specStatus == 'wv') {
+		if (document.respec) {		
+			document.respec.ready.then(function () {				
+			 addUrlsToDl();
+			});
+		}
+		else {	   
+			addUrlsToDl();		
+		}
+	}
+}; 
 
-// We gaan van achter naar voren toevoegen, zodat de eerste bovenaan komt
-for (let i = items.length - 1; i >= 0; i--) {
-    const item = items[i];
+function addUrlsToDl() {
+	// Selecteer de eerste <dl> in het document
+	const dl = document.querySelector('dl');
 
-    // Maak <dt>
-    const dt = document.createElement('dt');
-    dt.textContent = item.term;
+	console.log(respecConfig);
 
-    // Maak <dd>
-    const dd = document.createElement('dd');
-    const a = document.createElement('a');
-    a.href = item.url;
-    a.textContent = item.url;
+	// Maak de nieuwe elementen aan
+	const items = [];
+	
+	const specType = `${respecConfig.specType}`.toLowerCase();
+	
+	if (respecConfig.specStatus !== 'wv' ||  respecConfig.specStatus !== 'WV' ) {
+		const rawPublishDate = respecConfig.publishISODate.toLocaleString().split('T')[0]		
+							?? respecConfig.dashDate
+							?? new Date().toISOString().split('T')[0];
 
-    // Eventueel klasse toevoegen als het de 'Deze versie' is
-    if (item.term === 'Deze versie:') {
-        dd.className = 'status';
-        a.className = 'u-url status';
-    }
+		const publishDate = rawPublishDate.replace(/-/g, '');
+		const specStatus = `${respecConfig.specStatus}`.toLowerCase();
 
+		const docName = `${specStatus}-${specType}-${respecConfig.shortName}-${publishDate}`;
+
+		items.push({ term: 'Deze versie:', url: `https://docs.geostandaarden.nl/${respecConfig.pubDomain}/${docName}` });
+	}
+
+	if (respecConfig.previousPublishDate && respecConfig.previousMaturity) {
+		const previousMaturity = `${respecConfig.previousMaturity}`.toLowerCase();		
+		const previousPublishDate = new Date(respecConfig.previousPublishDate).toISOString().toLocaleString().split('T')[0].replace(/-/g, '');
+		const previousDocName = `${previousMaturity}-${specType}-${respecConfig.shortName}-${previousPublishDate}`;
+
+		items.push({ term: 'Vorige versie:', url: `https://docs.geostandaarden.nl/${respecConfig.pubDomain}/${previousDocName}` });
+	}
+	
+	items.push( { term: 'Laatst gepubliceerde versie ', url: `https://docs.geostandaarden.nl/${respecConfig.pubDomain}/`});
+	
+	
+
+	
+	// We gaan van achter naar voren toevoegen, zodat de eerste bovenaan komt
+	for (let i = items.length - 1; i >= 0; i--) {
+		const item = items[i];
+
+		// Maak <dt>
+		const dt = document.createElement('dt');
+		dt.textContent = item.term;
+
+		// Maak <dd>
+		const dd = document.createElement('dd');
+		const a = document.createElement('a');
+		a.href = item.url;
+		a.textContent = item.url;
+
+		// Eventueel klasse toevoegen als het de 'Deze versie' is
+		if (item.term === 'Deze versie:') {
+			dd.className = 'status';
+			a.className = 'u-url status';
+		}
+
+		dd.appendChild(a);
+		
+		console.log(dl);
+
+		// Prepend in omgekeerde volgorde
+		dl.insertBefore(dd, dl.firstChild);
+		dl.insertBefore(dt, dl.firstChild);
+	}
+
+}
